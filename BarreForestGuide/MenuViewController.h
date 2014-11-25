aab832c14d88522cd544dd23878e6aafea8e0316
Grace Ahmed <graceahmed@Graces-MacBook-Pro-5.local>
2014-11-25 15:16:55 -0500

    Menu View Controller
    Empty .m and .h files to eventually get the calling functionality
    attached to

---
+++ BarreForestGuide/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  BarreForestGuide
+//
+//  Created by Grace Ahmed on 11/25/14.
+//  Copyright (c) 2014 Town of Barre. All rights reserved.
+//
+
+#ifndef BarreForestGuide_MenuViewController_h
+#define BarreForestGuide_MenuViewController_h
+
+
+#endif
---
+++ BarreForestGuide/MenuViewController.m
@@ -0,0 +1,9 @@
+//
+//  MenuViewController.m
+//  BarreForestGuide
+//
+//  Created by Grace Ahmed on 11/25/14.
+//  Copyright (c) 2014 Town of Barre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
