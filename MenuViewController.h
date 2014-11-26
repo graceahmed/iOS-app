40cacf43067cf7b02e52b7f3fd352424d7d5de73
Grace Ahmed <graceahmed@Graces-MacBook-Pro-5.local>
2014-11-26 12:59:14 -0500

    Phone call not quite working
    Got the navigation to About view corrected again.
    
    Put code in to make a phone call (my home number is in there for
    testing) but when I run it on my phone, it’s not working yet.

---
+++ MenuViewController.h
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.h
+//  BarreForestGuide
+//
+//  Created by Grace Ahmed on 11/26/14.
+//  Copyright (c) 2014 Town of Barre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UITableViewController
+
+-(IBAction)phoneCall;
+
+@end
