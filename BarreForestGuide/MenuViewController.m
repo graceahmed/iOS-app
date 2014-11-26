70cc27c44fd70423099c6ec0e009554121117426
Grace Ahmed <graceahmed@Graces-MacBook-Pro-5.local>
2014-11-25 21:48:46 -0500

    Revert "Menu View Controller"
    This reverts commit aab832c14d88522cd544dd23878e6aafea8e0316.

--- BarreForestGuide/MenuViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  MenuViewController.m
-//  BarreForestGuide
-//
-//  Created by Grace Ahmed on 11/25/14.
-//  Copyright (c) 2014 Town of Barre. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
