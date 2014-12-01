7fde02e4721e43f524e83977d162b467fc9981e4
Craig B. Agricola <craig@theagricolas.org>
2014-12-01 07:01:10 -0800

    Fixed size/re-size issues with the UIWebView for the POI Details page
    It's still not perfect, but I think it's mostly resolved

--- BarreForestGuide/WebKitViewController.m
+++ BarreForestGuide/WebKitViewController.m
@@ -15,10 +15,37 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  self.webKit.delegate = self;
+  //NSLog(@"self.view.frame=(%f,%f,%f,%f)",self.view.frame.origin.x,self.view.frame.origin.y,self.view.frame.size.width,self.view.frame.size.height);
+  //NSLog(@"webKit.frame=(%f,%f,%f,%f)",self.webKit.frame.origin.x,self.webKit.frame.origin.y,self.webKit.frame.size.width,self.webKit.frame.size.height);
+  self.webKit.frame = self.view.frame;
+  self.webKit.scalesPageToFit = YES;
+}
 
+- (void)viewWillAppear:(BOOL)animated {
   if (self.url) {
+    NSLog(@"viewWillAppear: loadRequest(%@)", self.url);
     [self.webKit loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.url]]];
   }
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size
+       withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+  //NSLog(@"viewWillTransitionToSize: size=(%f,%f)",size.width,size.height);
+}
+
+- (void)webViewDidFinishLoad:(UIWebView*)webview
+{
+  CGSize contentSize = webview.scrollView.contentSize;
+  CGSize viewSize = self.view.bounds.size;
+
+  float rw = viewSize.width / contentSize.width;
+
+  //NSLog(@"webViewDidFinishLoad: contentSize=(%f,%f), viewSize=(%f,%f), rw=%f", contentSize.width, contentSize.height, viewSize.width, viewSize.height, rw);
+
+  webview.scrollView.minimumZoomScale = rw;
+  webview.scrollView.zoomScale = rw;
 }
 
 @end
